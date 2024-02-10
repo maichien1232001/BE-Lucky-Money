2fcfc6ba7c618e61a099c344fdae2bc32fba5997
Mai Chien <maichien.edu.com@gmail.com>
2024-02-10 22:09:49 +0700

    add

--- api.js
+++ api.js
@@ -12,35 +12,30 @@ const ClickedUser = require('./clicker');
 const app = express();
 const PORT = 8080;
 
-// Kết nối tới MongoDB
-mongoose.connect('mongodb://localhost:27017/lucky', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', async function () {
-  console.log("Connected to MongoDB");
+const uri = 'mongodb+srv://luckymoney:<1232001Truc>@cluster0.hcqcnwe.mongodb.net/?retryWrites=true&w=majority'
 
-  // Kiểm tra xem hệ thống đã tồn tại chưa, nếu chưa thì tạo mới
-  const existingSystem = await System.findOne({});
-  if (!existingSystem) {
-    const newSystem = new System({
-      totalMoney: 700,
-      totalUsers: 24,
-      zeroCount: 2,
-      isFinished: false // Thêm trường isFinished để kiểm tra xem quá trình đã kết thúc chưa
-    });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(async () => {
+    const existingSystem = await System.findOne({});
+    if (!existingSystem) {
+      const newSystem = new System({
+        totalMoney: 700,
+        totalUsers: 24,
+        zeroCount: 2,
+        isFinished: false
+      });
+      await newSystem.save();
+      console.log("Initial system data created");
+    } else {
+      console.log("System data already exists");
+    }
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
-    await newSystem.save();
-    console.log("Initial system data created");
-  } else {
-    console.log("System data already exists");
-  }
-});
-
-// Áp dụng middleware CORS và bodyParser
 app.use(cors());
 app.use(bodyParser.json());
 
-// API để lấy giá trị totalMoney
 app.get('/total-money', async (req, res) => {
   try {
     const systemInfo = await System.findOne({});
@@ -50,7 +45,6 @@ app.get('/total-money', async (req, res) => {
   }
 });
 
-// API để lấy giá trị totalUser
 app.get('/total-users', async (req, res) => {
   try {
     const systemInfo = await System.findOne({});
@@ -60,7 +54,6 @@ app.get('/total-users', async (req, res) => {
   }
 });
 
-// API để lấy thông tin của tất cả người dùng
 app.get('/users', async (req, res) => {
   try {
     const users = await User.find({});
@@ -70,8 +63,6 @@ app.get('/users', async (req, res) => {
   }
 });
 
-// Hàm để cập nhật thông tin hệ thống sau mỗi lần random
-// Hàm để cập nhật thông tin hệ thống sau mỗi lần random
 async function updateSystemInfo(newRandomAmount) {
   try {
     const systemInfo = await System.findOne({});
@@ -81,35 +72,29 @@ async function updateSystemInfo(newRandomAmount) {
       throw new Error("Quá trình random đã kết thúc.");
     }
 
-    let zeroCountIncrement = 0; // Biến để đếm số lượng người nhận 0
+    let zeroCountIncrement = 0; 
 
-    // Nếu random được số tiền 1000 và vẫn còn người nhận tiền
     if (newRandomAmount === 1000 && totalUsers > 0) {
       totalUsers--;
-    } else if (newRandomAmount === 0) { // Nếu random được số tiền 0
+    } else if (newRandomAmount === 0) {
       if (zeroCount < 2) {
         zeroCount++;
         zeroCountIncrement++;
       }
     } else {
-      // Chia số tiền còn lại cho các người dùng
-      const amountPerUser = totalMoney / (totalUsers - 3); // Trừ đi 3 vì đã cố định 2 người nhận 0 và 1 người nhận 1000
+      const amountPerUser = totalMoney / (totalUsers - 3);
       await User.updateMany({}, { $inc: { "denomination": amountPerUser } });
     }
 
-    // Cập nhật số tiền cho người nhận 0 và 1000
     await User.updateMany({ "denomination": 0 }, { $inc: { "denomination": 0 } }, { multi: true });
     await User.updateOne({ "denomination": 0 }, { $inc: { "denomination": 1000 } });
 
-    // Cập nhật số tiền cho người nhận 1000
     await User.updateOne({ "denomination": 1000 }, { $inc: { "denomination": newRandomAmount - 1000 } });
 
-    // Giảm số người dùng đi 1
     totalUsers--;
     if(totalUsers === 18){
       newRandomAmount = 0
     }
-    // Cập nhật thông tin hệ thống
     totalMoney -= (newRandomAmount - 1000);
     if (totalMoney <= 0 || totalUsers === 0) {
       isFinished = true;
@@ -122,23 +107,17 @@ async function updateSystemInfo(newRandomAmount) {
   }
 }
 
-// API endpoint để lưu thông tin người dùng và kết quả random vào cơ sở dữ liệu
-// API endpoint để lưu thông tin người dùng và kết quả random vào cơ sở dữ liệu
 app.post('/save-user-result', async (req, res) => {
   try {
     const { name } = req.body;
 
-    // Kiểm tra xem người dùng đã click trước đó chưa
     const existingUser = await ClickedUser.findOne({ name });
     if (existingUser) {
       return res.status(400).json({ message: "You have already clicked before." });
     }
 
-    // Lưu thông tin người dùng vào cơ sở dữ liệu
     const newUser = new ClickedUser({ name });
     await newUser.save();
-
-    // Tiếp tục xử lý và lưu kết quả random vào cơ sở dữ liệu nếu cần
 
     res.status(200).json({ message: "User information saved successfully" });
   } catch (error) {
@@ -146,30 +125,13 @@ app.post('/save-user-result', async (req, res) => {
   }
 });
 
-
-
-// API để thực hiện quá trình random
 app.post('/random-update', async (req, res) => {
   try {
     const systemInfo = await System.findOne({});
-    let { totalMoney } = systemInfo;
     let newRandomAmount;
-
-    // Xác định mức giới hạn mới dựa trên số tiền hiện có
-    // if (totalMoney < 500000) {
-    //   newRandomAmount = Math.floor(Math.random() * (100000 - 10000 + 1)) + 10000;
-    // } else if (totalMoney < 400000) {
-    //   newRandomAmount = Math.floor(Math.random() * (80000 - 10000 + 1)) + 10000;
-    // } else if (totalMoney < 300000) {
-    //   newRandomAmount = Math.floor(Math.random() * (50000 - 10000 + 1)) + 10000;
-    // } else {
-    //   newRandomAmount = Math.floor(Math.random() * (200000 - 10000 + 1)) + 10000;
-    // }
 
       newRandomAmount = Math.floor(Math.random() * (50000 - 10000 + 1)) + 10000;
     const newTotalMoney = await updateSystemInfo(newRandomAmount);
-
-    // Lưu kết quả vào MongoDB
     const result = new Result({
       randomAmount: newRandomAmount,
       newTotalMoney: newTotalMoney
@@ -183,7 +145,6 @@ app.post('/random-update', async (req, res) => {
 });
 
 
-// Khởi động server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
